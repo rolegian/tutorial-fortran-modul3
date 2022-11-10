28974e6c6c30e64cb9402d09848ae8f85282c0f8
Rolegian <mail.alfren@gmail.com>
2022-11-10 12:49:56 +0700

    membuat program inputan

--- KOMPOS_MOODUL3_12921010.for
+++ KOMPOS_MOODUL3_12921010.for
@@ -1,19 +1,32 @@
 	program matriks
 	integer:: i, j
 	real:: result
-	real, dimension(3,3):: matA
+	real, dimension(3,3):: matA, matB
 
 	write(*,*) 'Masukkan nilai matriks A:'
-	
+ 
 
 	do i=1, 3
 		do j=1, 3
-			write(*,'(1X,A,I2,A,I2,A,\)')'B(',i,',',j,');'
+			write(*,'(1X,A,I2,A,I2,A,\)')'A(',i,',',j,');'
 			read(*,*) matA(i,j)
 		enddo
 	enddo
 	do i=1,3
 	write(*,*) (matA (i,j), j=1,3) 
 	enddo
-	
+
+	write(*,*) 'Masukkan nilai matriks B:'
+
+	do i=1, 3
+		do j=1, 3
+			write(*,'(1X,A,I2,A,I2,A,\)')'B(',i,',',j,');'
+			read(*,*) matB(i,j)
+		enddo
+	enddo
+	do i=1,3
+	write(*,*) (matB (i,j), j=1,3) 
+	enddo
+
+
 	end
